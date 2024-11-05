333c41c77a578e081b3c36918a3ed9eea3de22b9
aleksaqm <perovic.aleksa03@gmail.com>
2024-11-05 10:22:53 +0100

    feat: created user and meeting models

--- server/model/user.go
+++ server/model/user.go
@@ -1,7 +1,29 @@
 package model
 
+type AccountStatus int
+
+const (
+	Active AccountStatus = iota
+	Inactive
+	Suspended
+)
+
+type Role int
+
+const (
+	Regular Role = iota
+	Clerk
+	Admin
+	SuperAdmin
+)
+
 type User struct {
-	Id        uint16
-	FirstName string
-	LastName  string
+	Id           uint64        `gorm:"primary_key" json:"id"`
+	Username     string        `gorm:"unique" json:"username"`
+	Password     string        `json:"password,omitempty"`
+	Email        string        `gorm:"unique" json:"email"`
+	ProfileImage string        `json:"profileImage,omitempty"`
+	Status       AccountStatus `json:"status"`
+	Role         Role          `json:"role"`
+	Meetings     []Meeting     `gorm:"foreignKey:ClerkID;foreignKey:UserID" json:"meetings,omitempty"` //maybe will need to separate clarkMeetings and userMeetings
 }
